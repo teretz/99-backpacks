fcab93e9df4069c502aed830c5990c2d212920a1
Jason James <teretz@gmail.com>
2023-08-14 00:33:27 -0600

    Revert "Initial commit; looks like duplicates occured since  kaguya didnt erase after converting to c#. cleanup on the way w mentat."
    This reverts commit 6e5e5cf8f21287f63a51583ebfca9f5aa599fefb.

--- inventory_system.js
+++
@@ -1,8 +0,0 @@
-function handleItemWeight(inventory) {
-  // Logic to handle item weight in the inventory
-  let totalWeight = 0;
-  inventory.forEach(item => totalWeight += item.weight);
-  return totalWeight <= MAX_WEIGHT;
-}
-
-// Other inventory system functions
