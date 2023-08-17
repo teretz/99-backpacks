5990bec04ead3f0e0d3f157cd5334b75b3d367d7
Jason James <teretz@gmail.com>
2023-08-17 15:10:37 -0500

    more fixing stupid bugs

--- prefab_factory/prefab_factory.cs
+++ prefab_factory/prefab_factory.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using System.Collections.Generic;
 
 public class PrefabFactory : MonoBehaviour
@@ -25,8 +28,32 @@ public class PrefabFactory : MonoBehaviour
             prefab.GetComponent<Rigidbody>().material = template.physicMaterial;
             prefab.transform.localScale = new Vector3(template.size, template.size, template.size);
             if (template.isTwoHanded) { /* Apply two-handed logic */ }
+
+            AttachGrabbableComponent(prefab); // Attach grabbable component
+
             generatedPrefabs.Add(prefab);
         }
+    }
+
+    private void AttachGrabbableComponent(GameObject prefab)
+    {
+        // Assuming Grabbable is a component that handles grab functionality
+        Grabbable grabbable = prefab.AddComponent<Grabbable>();
+
+        // You can customize the grabbable component here if needed
+        // e.g., grabbable.someProperty = someValue;
+    }
+
+    public void SaveGeneratedPrefabs()
+    {
+#if UNITY_EDITOR
+        foreach (GameObject prefab in generatedPrefabs)
+        {
+            string path = "Assets/Prefabs/" + prefab.name + ".prefab";
+            PrefabUtility.SaveAsPrefabAsset(prefab, path);
+            Debug.Log("Saved prefab: " + path);
+        }
+#endif
     }
 
     // Additional methods for managing and customizing prefabs
